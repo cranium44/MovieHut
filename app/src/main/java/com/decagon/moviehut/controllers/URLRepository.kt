bb9851c2be2991ce6cc7a87eb3d822d6a2ccbc50
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-02-09 23:11:42 +0100

    added the base urls and api key

--- app/src/main/java/com/decagon/moviehut/controllers/URLRepository.kt
+++ app/src/main/java/com/decagon/moviehut/controllers/URLRepository.kt
@@ -1,4 +1,10 @@
 package com.decagon.moviehut.controllers
 
 interface URLRepository {
+    companion object{
+        const val API_BASE_URL = "https://api.themoviedb.org/3/movie"
+        const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
+        const val GENRE_BASE_URL = "https://api.themoviedb.org/3/genre/"
+        const val API_KEY = "28dda9f76d76f128b47831768bc9a103"
+    }
 }
