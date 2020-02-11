e200ae1f9f0b3d63cf099e3102b5fc90209997e8
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-02-11 02:15:38 +0100

    tweaked the layout of the recyclerview item

--- app/src/main/java/com/decagon/moviehut/views/DetailsFragment.kt
+++ app/src/main/java/com/decagon/moviehut/views/DetailsFragment.kt
@@ -1,14 +1,17 @@
 package com.decagon.moviehut.views
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.decagon.moviehut.R
+import com.decagon.moviehut.controllers.URLRepository
+import com.decagon.moviehut.data.movieresponse.Movie
 import com.decagon.moviehut.viewmodels.DetailsViewModel
+import kotlinx.android.synthetic.main.details_fragment.*
 
 class DetailsFragment : Fragment() {
 
@@ -22,13 +25,21 @@ class DetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.details_fragment, container, false)
+        val view = inflater.inflate(R.layout.details_fragment, container, false)
+
+        val movie: Movie = arguments!!.get("movie") as Movie
+        view.findViewById<TextView>(R.id.details_title).text = movie.title
+        view.findViewById<TextView>(R.id.details_overview).text = movie.overview
+        view.findViewById<TextView>(R.id.rating).text = movie.voteAverage.toString()
+        view.findViewById<TextView>(R.id.details_popularity).text = movie.popularity.toString()
+        Glide.with(context!!)
+            .load(URLRepository.IMAGE_BASE_URL + "original" + movie.backdropPath)
+            .into(view.findViewById(R.id.movie_poster))
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(DetailsViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
