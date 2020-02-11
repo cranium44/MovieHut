7e29442ec960b575c414621c107254f0697b0865
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-02-11 04:32:55 +0100

    implemented database in viewmodel

--- app/src/main/java/com/decagon/moviehut/viewmodels/DetailsViewModel.kt
+++ app/src/main/java/com/decagon/moviehut/viewmodels/DetailsViewModel.kt
@@ -1,14 +1,32 @@
 package com.decagon.moviehut.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.decagon.moviehut.controllers.DatabaseRepository
 import com.decagon.moviehut.controllers.NetworkRepository
+import com.decagon.moviehut.data.database.FavouriteMovie
 import com.decagon.moviehut.data.movieresponse.Movie
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class DetailsViewModel : ViewModel() {
     lateinit var movie: Movie
     private val repository = NetworkRepository
+    private val dbRepository = DatabaseRepository
 
-//    fun getMovies(): List<Movie>{
-//
-//    }
+    private val viewModelJob = Job()
+    private var viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    private val _favouriteMovies = MutableLiveData<List<FavouriteMovie>>()
+    val favouriteMovies
+        get() = _favouriteMovies
+
+
+    init {
+        viewModelScope.launch {
+            _favouriteMovies.value = dbRepository.getDataFromDatabase().value
+        }
+    }
 }
