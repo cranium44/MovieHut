9942fb4b963dad791ab7ff65a513ea0a64dfe0ca
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-02-11 15:31:32 +0100

    made utility function for saving image

--- app/src/main/java/com/decagon/moviehut/controllers/Utils.kt
+++ app/src/main/java/com/decagon/moviehut/controllers/Utils.kt
@@ -3,20 +3,24 @@ package com.decagon.moviehut.controllers
 import android.content.Context
 import android.graphics.Bitmap
 import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.FileOutputStream
 
 
 object Utils {
 
-    fun saveImage(context: Context, b: Bitmap, imageName: String?) {
-        val foStream: FileOutputStream
-        try {
-            foStream = context.openFileOutput(imageName, Context.MODE_PRIVATE)
-            b.compress(Bitmap.CompressFormat.PNG, 100, foStream)
-            foStream.close()
-        } catch (e: Exception) {
-            Log.d("saveImage", "Exception 2, Something went wrong!")
-            e.printStackTrace()
+    suspend fun saveImage(context: Context, b: Bitmap, imageName: String?) {
+        var foStream: FileOutputStream
+        withContext(Dispatchers.IO){
+            try {
+                foStream = context.openFileOutput(imageName, Context.MODE_PRIVATE)
+                b.compress(Bitmap.CompressFormat.PNG, 100, foStream)
+                foStream.close()
+            } catch (e: Exception) {
+                Log.d("saveImage", "Exception 2, Something went wrong!")
+                e.printStackTrace()
+            }
         }
     }
 }
