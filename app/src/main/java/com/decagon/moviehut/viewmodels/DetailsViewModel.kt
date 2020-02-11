7779f1827f0b449336103d2113dae5d596229e4b
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-02-11 05:01:58 +0100

    added database functionality to the viewmodels

--- app/src/main/java/com/decagon/moviehut/viewmodels/DetailsViewModel.kt
+++ app/src/main/java/com/decagon/moviehut/viewmodels/DetailsViewModel.kt
@@ -1,32 +1,34 @@
 package com.decagon.moviehut.viewmodels
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import com.decagon.moviehut.controllers.DatabaseRepository
-import com.decagon.moviehut.controllers.NetworkRepository
 import com.decagon.moviehut.data.database.FavouriteMovie
-import com.decagon.moviehut.data.movieresponse.Movie
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-class DetailsViewModel : ViewModel() {
-    lateinit var movie: Movie
-    private val repository = NetworkRepository
+class DetailsViewModel(application: Application) : AndroidViewModel(application) {
+    //private val repository = NetworkRepository
     private val dbRepository = DatabaseRepository
-
     private val viewModelJob = Job()
     private var viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
 
-    private val _favouriteMovies = MutableLiveData<List<FavouriteMovie>>()
-    val favouriteMovies
-        get() = _favouriteMovies
-
 
     init {
+        dbRepository.context = application.applicationContext
+    }
+
+    fun saveFavourite(favouriteMovie: FavouriteMovie){
         viewModelScope.launch {
-            _favouriteMovies.value = dbRepository.getDataFromDatabase().value
+            dbRepository.saveFavouriteMovie(favouriteMovie)
+        }
+    }
+
+    fun deleteFavourite(favouriteMovie: FavouriteMovie){
+        viewModelScope.launch {
+            dbRepository.deleteFavouriteMovie(favouriteMovie)
         }
     }
 }
--- app/src/main/java/com/decagon/moviehut/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/decagon/moviehut/viewmodels/HomeViewModel.kt
@@ -1,19 +1,20 @@
 package com.decagon.moviehut.viewmodels
 
-import android.util.Log
-import androidx.lifecycle.LiveData
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.decagon.moviehut.controllers.MoviesAdapter
+import com.decagon.moviehut.controllers.DatabaseRepository
 import com.decagon.moviehut.controllers.NetworkRepository
+import com.decagon.moviehut.data.database.FavouriteMovie
 import com.decagon.moviehut.data.movieresponse.Movie
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val dbRepository = DatabaseRepository
     private val repository = NetworkRepository
     private val viewModelJob = Job()
     private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
@@ -23,9 +24,15 @@ class HomeViewModel : ViewModel() {
         val allMovies
         get() = _allMovies
 
+    private val _favouriteMovies = MutableLiveData<List<FavouriteMovie>>()
+    val favouriteMovies
+        get() = _favouriteMovies
+
     init {
+        dbRepository.context = application.applicationContext
         viewModelScope.launch {
             _allMovies.value = repository.getMovies()
+            _favouriteMovies.value = dbRepository.getDataFromDatabase().value
         }
     }
 }
