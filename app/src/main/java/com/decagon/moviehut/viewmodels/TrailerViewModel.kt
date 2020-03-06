3aeb6ce396c259668be076035808f5292c2083a4
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-03-06 03:53:34 +0100

    implemented the trailer viewmodel

--- app/src/main/java/com/decagon/moviehut/viewmodels/TrailerViewModel.kt
+++ app/src/main/java/com/decagon/moviehut/viewmodels/TrailerViewModel.kt
@@ -1,6 +1,26 @@
 package com.decagon.moviehut.viewmodels
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import com.bumptech.glide.Glide.init
+import com.decagon.moviehut.controllers.repositories.NetworkRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-class TrailerViewModel: ViewModel() {
+class TrailerViewModel(application: Application): AndroidViewModel(application) {
+    private val networkRepository = NetworkRepository(application)
+    lateinit var key: String
+
+    //coroutine
+    private val job = Job()
+    private val viewModelScope = CoroutineScope(job + Dispatchers.Main)
+
+    fun getKey(movieId: Int){
+        viewModelScope.launch {
+            key = networkRepository.getTrailerUrls(movieId)
+        }
+    }
 }
